30e012ff23afd3894fce9f29730c2ebc9eb1d328
LeemanMF <167991135+LeemanMF@users.noreply.github.com>
2024-04-29 12:21:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 import numpy as np
-from control import matlab as cm
-import matplotlib.pyplot as plt
-import pandas as ps
-import SymPy as simp
+import control as ct
+#import matplotlib.pyplot as plt
+#import pandas as ps
+#import SymPy as simp
+
+## Variables
+g = 9.81
+l = 0.5 #m
+m = 5 #kg
+## Control matrices
+A = np.array([[0, 0, 1, 0],
+             [0, 0, 0, 1],
+             [0, -(g/l), 0,0],
+             [-(g/l), 0, 0, 0]]);
+
+B = np.array([[0, 0],
+             [0, 0],
+             [(1/m), 0],
+             [0, (1/m)]]);
+
+C = np.array([[1, 0, 0, 0],
+             [0, 1, 0, 0]]);
+
+D = np.array([0]);
+
+## Q matrix en R matrix
+
+Q = np.identity(4);
+
+R = np.identity(2);
+
+
+
+## LQR solver
+K, S, E = ct.lqr(A, B, Q, R);
+
+print(K);
