361d3988e02ac8befb17da5285db1a5297530caa
LeemanMF <167991135+LeemanMF@users.noreply.github.com>
2024-05-13 15:33:41 +0200

    Berts fouten verbeterd

--- Werkend/Python/Main.py
+++ Werkend/Python/Main.py
@@ -1,11 +1,11 @@
 #main file waarbij alle losse functies worden opgeroepen en uitgevoerd
 #nog niet getest
 
-from data import data
-from controller import LQR_control
-from Motoren import motor_control
+from Data import data
+from Controller import LQR_control
+from Motoren import motor_drive
 
 
 theta,z,theta_dot,zdot = data()
 Fmotor1 = LQR_control(theta, theta_dot)
-speed = motor_control(Fmotor1)
+speed = motor_drive(Fmotor1, theta_dot)
