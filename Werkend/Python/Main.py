c5bbf4bb69950c1b3feb20e8328352ecb2b34af7
LeemanMF <167991135+LeemanMF@users.noreply.github.com>
2024-05-13 13:00:13 +0200

    Functieverwijzing gefixt

--- Werkend/Python/Main.py
+++ Werkend/Python/Main.py
@@ -1,11 +1,11 @@
 #main file waarbij alle losse functies worden opgeroepen en uitgevoerd
 #nog niet getest
 
-from data import data
+from Data import data
 print(data())
 
-from controller import LQR_control
+from Controller import LQR_control
 print(LQR_control())
 
-from Motoren import motor_control
-print(motor_control())
+from Motoren import motor_drive
+print(motor_drive())
