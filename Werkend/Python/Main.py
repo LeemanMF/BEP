981d6758b210561797440f8b919b828b166cf6c4
jikielstra <167991474+jikielstra@users.noreply.github.com>
2024-05-13 16:23:31 +0200

    met motor aansturing

--- Werkend/Python/Main.py
+++ Werkend/Python/Main.py
@@ -1,6 +1,6 @@
 #main file waarbij alle losse functies worden opgeroepen en uitgevoerd
 #nog niet getest
-
+import serial
 from Data import data
 from Controller import LQR_control
 from Motoren import motor_drive
@@ -9,3 +9,6 @@ from Motoren import motor_drive
 theta,z,theta_dot,zdot = data()
 Fmotor1 = LQR_control(theta, theta_dot)
 speed = motor_drive(Fmotor1, theta_dot)
+
+Motor = serial.Serial('COM7',9600) #connectie met Arduino
+Motor.write(b'speed')
