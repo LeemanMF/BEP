1b2c845ebd179220e36013d931fb682622198189
jikielstra <167991474+jikielstra@users.noreply.github.com>
2024-05-13 11:14:42 +0200

    omgeschreven met y=theta en ydot=theta_dot

--- Werkend/Python/Data.py
+++ Werkend/Python/Data.py
@@ -5,22 +5,25 @@
 import time
 import serial #import van programma om arduino uit te lezen
 
-arduinoData=serial.Serial('com7',115200) # juiste arduino selecteren
-time.sleep(1)
-while (True):   #zorgen dat python info blijft ophalen
-    with arduinoData:
-        while (arduinoData.inWaiting()==0): #combineert python en arduino op de arduino, voorkomt afsluiten
-            pass
-        dataPacket = arduinoData.readline() #uitlezen van data
-        #dataPacket=str(dataPacket,'utf-8')
-        dataPacket = dataPacket.decode('utf-8').strip() #data is vorm zetten zonder 'b en 'n/r
-        #print(dataPacket)
-        splitPacket=dataPacket.split(",") #losse variabelen selecteren
-        #print(splitPacket)
-        #print(type (splitPacket))
-        #time.sleep(.2)
-        y=float(splitPacket[0]) #los variabel oproepen als float
-        z=float(splitPacket[1])
-        ydot=float(splitPacket[2])
-        zdot=float(splitPacket[3])
-        print ("y=",y," z=",z," ydot=",ydot, "zdot=",zdot) # print variabelen
+def data():
+    arduinoData=serial.Serial('com7',115200) # juiste arduino selecteren
+    time.sleep(1)
+    while (True):   #zorgen dat python info blijft ophalen
+        with arduinoData:
+            while (arduinoData.inWaiting()==0): #combineert python en arduino op de arduino, voorkomt afsluiten
+                pass
+            dataPacket = arduinoData.readline() #uitlezen van data
+            #dataPacket=str(dataPacket,'utf-8')
+            dataPacket = dataPacket.decode('utf-8').strip() #data is vorm zetten zonder 'b en 'n/r
+            #print(dataPacket)
+            splitPacket=dataPacket.split(",") #losse variabelen selecteren
+            #print(splitPacket)
+            #print(type (splitPacket))
+            #time.sleep(.2)
+            theta=float(splitPacket[0]) #los variabel oproepen als float
+            z=float(splitPacket[1])
+            theta_dot=float(splitPacket[2])
+            zdot=float(splitPacket[3])
+            print ("theta=",theta," z=",z," theta_dot=",theta_dot, "zdot=",zdot) # print variabelen
+        return theta,z,theta_dot,zdot
+
