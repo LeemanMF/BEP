16d43b80d3a67f544661eea1a4a71ee862841d6e
LeemanMF <167991135+LeemanMF@users.noreply.github.com>
2024-05-14 11:57:10 +0200

    .

--- Werkend/Python/Motoren.py
+++ Werkend/Python/Motoren.py
@@ -3,16 +3,21 @@
 #Wellicht omschrijven naar Voltage outputs
 
 #ToDO: Maximale kracht op touw inbouwen, failsafe inbouwen als motor sneller moet draaien dan hij kan
-straal_motor = 1.0 #cm 
+#Check: Wellicht moet motor_drive soms + en soms - elkaar!
+import numpy as np
+
+#RPM of rad/s?
+straal_motor = 0.01 #m 
 ts_ratio = 0.2 #torque speed ratio
-no_load_speed = 160 #rpm zonder load
-stall_torque = 0.8 #kg/cm
+no_load_speed = 160 * 2 * np.pi / 60 #rad/s zonder load
+stall_torque = 0.8 * 9.81 * 0.01 #N/m
+rated_torque = 0.2 * 9.81 * 0.01 #N/m
 
 #Functie om de motor kracht te laten uitoefenen
 def motor_control(Fmotor1):
     torque_needed = Fmotor1 / straal_motor
-    if torque_needed > 0.2 : #kg/cm, als hoger dan de rated torque (maximale werktorque), zet gelijk aan rated torque
-        torque_needed = 0.2 * straal_motor 
+    if torque_needed > rated_torque : #N/m, als hoger dan de rated torque (maximale werktorque), zet gelijk aan rated torque
+        torque_needed = rated_torque * straal_motor 
     force_motor_speed = no_load_speed - (no_load_speed * (1 - torque_needed / stall_torque)) 
     return force_motor_speed
 
@@ -31,8 +36,8 @@ def motor_drive(Fmotor1, theta_dot):
 
 
 #test
-Fmotor1example = 0.001 #N
-theta_dotexample = 5 #
+Fmotor1example = 0.06 #N
+theta_dotexample = -5 #
 print("Motor follow",motor_follow(theta_dotexample));
 print("Motor kracht",motor_control(Fmotor1example));
-print(motor_drive(Fmotor1example, theta_dotexample)); 
+print("Totaal",motor_drive(Fmotor1example, theta_dotexample)); 
